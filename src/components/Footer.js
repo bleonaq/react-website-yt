5bb9c549fb3efe98e495d0ac215d9e0fd92b912c
bleonaq <74006594+bleonaq@users.noreply.github.com>
2021-05-31 14:17:57 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,7 +39,6 @@ function Footer() {
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
             <Link to='/'>Sponsorships</Link>
           </div>
         </div>
@@ -48,8 +47,6 @@ function Footer() {
             <h2>Videos</h2>
             <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Social Media</h2>
@@ -64,11 +61,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              UBT
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>UBT © 2020</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
